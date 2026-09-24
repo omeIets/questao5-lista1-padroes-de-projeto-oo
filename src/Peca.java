61e3605dd4f89484b00cb4fccc223c564900e9e9
lets :) <157411273+omeIets@users.noreply.github.com>
2026-09-24 00:37:22 -0300

    feat: implementa a classe Peca representando os nos folhas indivisiveis

--- src/Peca.java
+++ src/Peca.java
@@ -1,2 +1,13 @@
-public class Peca {
+public class Peca extends ComponenteCarro {
+
+    public Peca(String nome, double peso) {
+        super(nome, peso);
+    }
+
+    @Override
+    public double calcularPeso() {
+        somaParcial += this.peso;
+        System.out.println("Somando agora o peso de " + this.nome + ": " + this.peso + ". Total parcial: " + somaParcial);
+        return this.peso;
+    }
 }
