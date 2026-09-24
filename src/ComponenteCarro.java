d483a6a9d4c6f7cf1b148d4090a2d88421f6d02c
lets :) <157411273+omeIets@users.noreply.github.com>
2026-09-24 00:34:03 -0300

    feat: cria classe abstrata base do Composite com controle de variaveis de peso

--- src/ComponenteCarro.java
+++ src/ComponenteCarro.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public abstract class ComponenteCarro {
+    protected String nome;
+    protected double peso;
 
-public class ComponenteCarro {
+    // Variável estática para compartilhar a soma entre todas as instâncias no console
+    protected static double somaParcial = 0;
+
+    public ComponenteCarro(String nome, double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+    public abstract double calcularPeso();
 }
